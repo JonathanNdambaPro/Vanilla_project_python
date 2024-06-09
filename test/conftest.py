f76bcdb077c3012357a8b4776913d7d5ea511d98
jojopvcp <jonathan.ndamba@ext.groupepvcp.com>
2024-06-09 18:40:12 +0200

    first commit

---
+++ test/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from dataclasses import dataclass
+
+@pytest.fixture
+def mock_url_valid():
+    return "https://catfact.ninja/fact"
+
+@pytest.fixture
+def mock_url_no_valid():
+    return "https://www.facebook.com/"
+
+@pytest.fixture
+def mock_reponse_valid():
+    @dataclass
+    class Response:
+        status_code: int = 200
+
+        def json(*_, **__):
+            return {"lol": "lol"}
+        
+    response = Response()
+
+    return response
+
+@pytest.fixture
+def mock_reponse_no_valid(mock_reponse_valid):
+    mock_reponse_valid.status_code = 404
+    return mock_reponse_valid
+
+
+
